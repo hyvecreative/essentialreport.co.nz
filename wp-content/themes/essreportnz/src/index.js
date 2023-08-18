c93a285e906fe7c8d3442e11c3d823a29cbc4a3b
Andrew <andrew@hyve.com.au>
2023-08-18 16:33:48 +1000

    theme update

--- wp-content/themes/essreportnz/src/index.js
+++ wp-content/themes/essreportnz/src/index.js
@@ -1,43 +1,12 @@
 import $ from 'jquery';
 import './style.scss';
 import HeaderSearch from './js/components/header-search';
-import Carousel from './js/components/carousel';
-import matchHeight from 'jquery-match-height';
 import scrollTo from 'jquery.scrollto';
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
 
 $(() => {
 
   const headerSearch = new HeaderSearch();
   headerSearch.init();
-
-  const carousel = new Carousel();
-  carousel.init();
-
-  // Match height
-  $('.sub-navs').matchHeight('true');
-  $('.fiveitem').matchHeight('true');
-  $('.support-opt-wrap').matchHeight({target: $('.support-opt')});
-
-  const $grid = $('.grid');
-
-  if ($grid.length) {
-
-    const gridMasonry = new Masonry('.grid', {
-      // set itemSelector so .grid-sizer is not used in layout
-      itemSelector: '.grid-item',
-      // use element for option
-      percentPosition: true,
-      // columnWidth: '.grid-sizer'
-    });
-
-    // layout Masonry after each image loads
-    imagesLoaded.makeJQueryPlugin( $ );
-    $grid.imagesLoaded(function() {
-      gridMasonry.layout();
-    });
-  	}
 	
 	// Scrollto
 	
@@ -58,19 +27,34 @@ $(() => {
 	// Activate animation on scroll 
 	
 	function check_from_top_de(){ // Create our function
-    var scroll = $(window).scrollTop(); // Check scroll distance
-    if (scroll >= 300) { // If it is equal or more than 300 - you can change this to what you want
-      $("#figure").addClass("animate"); // Add custom class to body
-    } else {
-      $("#figure").removeClass("animate"); // When scrolled to the top, remove the class
-    }
+		var scroll = $(window).scrollTop(); // Check scroll distance
+		if (scroll >= 300) { // If it is equal or more than 300 - you can change this to what you want
+		  $("#figure").addClass("animate"); // Add custom class to body
+		} else {
+		  $("#figure").removeClass("animate"); // When scrolled to the top, remove the class
+		}
+  	}
+  
+	  check_from_top_de(); // On load, run the function
+
+	  $(window).scroll(function() { // When scroll - run function
+		check_from_top_de();
+	  });
+	
+	// Animation for nav hamburger
+	
+
+	const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
   }
-  
-  check_from_top_de(); // On load, run the function
-  
-  $(window).scroll(function() { // When scroll - run function
-    check_from_top_de();
-  });
+});
 
     
 });
